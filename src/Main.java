c60ad2aa6912b385c52f47789ee89d72fd48e0cc
Maria Negretskaya <marian.93@mail.ru>
2022-05-01 16:45:12 +0300

    Data account

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int primaryBalance = 100;
+        int sumOfFirstRefill = 100;
+        int sumOfSecondRefill = 1100;
+        int bonus = sumOfSecondRefill / 100;
+        System.out.println("Итоговый счет1");
+        System.out.println((primaryBalance + sumOfFirstRefill) + " руб ");
+        System.out.println("Итоговый счет2");
+        System.out.println((primaryBalance + sumOfSecondRefill + bonus) + " руб ");
+
+
+        System.out.println("Количество бонусных рублей2");
+        System.out.println((sumOfSecondRefill / 100) + " руб ");
+    }
+
+
+}
