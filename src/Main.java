1a6eac0f05d817969f9bd52e410d42a1b1c229e0
Maria Negretskaya <marian.93@mail.ru>
2022-05-03 16:36:07 +0300

    Corrections sum of binus

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,6 @@ public class Main {
         int sumOfSecondRefill = 1100;
         int bonus = sumOfSecondRefill / 100;
 
-        System.out.println("Итоговый счет2");
-        System.out.println((primaryBalance + sumOfSecondRefill + bonus) + " руб ");
-
-
         System.out.println("Количество бонусных рублей2");
         System.out.println((sumOfSecondRefill / 100) + " руб ");
     }
