1352ff8a0bf40283f6b0a167d2bfd15d66f004a0
Maria Negretskaya <marian.93@mail.ru>
2022-05-04 19:30:56 +0300

    New corrections

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         int primaryBalance = 100;
-        int sumOfFirstRefill = 100;
         int sumOfSecondRefill = 1100;
         int bonus = sumOfSecondRefill / 100;
 
