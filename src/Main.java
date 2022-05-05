ff3c62faa9ec83aa2a0ec45fccdd5cbec4e4c3ca
Mariya Negretskaya <marian.93@mail.ru>
2022-05-05 20:49:12 +0300

    Bonus calculation

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,19 @@ public class Main {
         int primaryBalance = 100;
         int sumOfSecondRefill = 1100;
         int bonus = sumOfSecondRefill / 100;
+        if (sumOfSecondRefill > 1000) {
+            bonus = sumOfSecondRefill / 100;
+
+        } else {
+            bonus =0;
+        }
+
 
         System.out.println("Количество бонусных рублей2");
         System.out.println((sumOfSecondRefill / 100) + " руб ");
+        System.out.println("Итоговая сумма");
+        System.out.println((primaryBalance + sumOfSecondRefill) + " руб ");
+
     }
 
 
